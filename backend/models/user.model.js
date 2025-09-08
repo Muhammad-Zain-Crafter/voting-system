36118223901ea39174a8460c1472e71759a01efe
Muhammad-Zain-Crafter <zmunawar872@gmail.com>
2025-09-08 15:34:07 -0700

    Add user schema with fields for username, age, email, fullName, cnicNumber, password, role, and isVoted

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,6 +2,51 @@ import mongoose, { mongo, Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-const userSchema = new Schema({ 
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    lowercase: true,
+    trim: true,
+  },
 
-})
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
+  cnicNumber: {
+    type: String,
+    required: true,
+    unique: true, // one CNIC = one vote
+    trim: true,
+    match: [/^\d{13}$/, "CNIC must be 13 digits"],
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 18,
+  },
+  role: {
+    type: String,
+    enum: ["voter", "admin"],
+    default: "voter",
+  },
+  isVoted: {
+    type: Boolean,
+    default: false,
+  },
+});
